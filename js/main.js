67fc30642adc19d05dc36a3f96a8e74f6e2e14b1
Jeffrey Martín <jefdonmar@gmail.com>
2016-01-08 01:35:22 -0500

    Taking a break until tomorrow

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
+import $ from 'jquery';
+import _ from 'underscore';
+import moment from 'moment';
+
+
 (function () {
+
+  let etsyURL = 'https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop';
+
+  $.ajax({
+    url: etsyURL,
+    dataType: 'jsonp',
+    method: 'get'
+  }).then (function (response) {
+    console.log(response);
+  });
 
   var templateString = $('#itemTemplate').text();
 
