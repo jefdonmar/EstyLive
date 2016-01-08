d7701e367fd3d21fa19dd49b122254a8a94257b8
Jeffrey Martín <jefdonmar@gmail.com>
2016-01-08 01:19:28 -0500

    Added my code from the Esty Clone and am not starting to work with the AJAX and esty url of the project

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-import $ from 'jquery';
-import _ from 'underscore';
-import moment from 'moment';
+(function () {
 
-console.log('Hello, World');
+  var templateString = $('#itemTemplate').text();
+
+  var renderTemplate = _.template(templateString);
+
+  // bring up items I want
+
+
+  _.each( etsy.results, function(item){
+    var itemInfo = renderTemplate(item);
+    $('.results-area').append(itemInfo);
+  });
+
+}());
